5b845e5851b695001e70e4a39502f58cdfb69d3e
unknown <henry@novap.legal>
2020-09-23 21:27:45 -0500

    Create hello.js

--- src/init/index.ts
+++ src/init/index.ts
@@ -5,6 +5,7 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 // per file.
 export function init(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    tree.create('hello.js', `console.log('Frontend Rules');`);
     return tree;
   };
 }

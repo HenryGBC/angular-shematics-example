8f5d324d196a8fe413cc5a30c3c460fc06229040
unknown <henry@novap.legal>
2020-09-24 20:56:53 -0500

    Add template

---
+++ src/init/files/hello-__name@dasherize__/hello-__name@dasherize__.ts
@@ -0,0 +1,1 @@
+console.log('Hi <%= name %> Frontend Rules');
--- src/init/index.ts
+++ src/init/index.ts
@@ -1,11 +1,20 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
+import { apply, mergeWith, Rule, SchematicContext, template, Tree, url } from '@angular-devkit/schematics';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function init(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    tree.create('hello.js', `console.log('Hi ${_options.name}Frontend Rules');`);
-    return tree;
+    const sourceTemplates =  url('./files');
+    const sourceParametrizedTemplates = apply(sourceTemplates, [
+      template({
+        ..._options,
+        ...strings
+      })
+    ]);
+
+    return mergeWith(sourceParametrizedTemplates)(tree, _context);
+    
   };
 }
